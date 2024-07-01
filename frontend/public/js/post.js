a9efb35355d9c3d907360790fd6e90c4cb323929
true <truee030@gmail.com>
2024-07-01 16:20:27 +0900

    🐛 게시글 목록 에러 수정

--- frontend/public/js/post.js
+++ frontend/public/js/post.js
@@ -14,11 +14,12 @@
 
     const responseData = await response.json();
     console.log('Response Data:', responseData); // 반환된 JSON 데이터 구조 확인
+
     const posts = responseData.data;
 
     const boardContainer = document.querySelector('.board-container');
 
-    if (posts.length === 0) {
+    if (!posts || posts.length === 0) {
       const needPost = document.createElement('div');
       needPost.textContent = '게시글이 없습니다';
       boardContainer.appendChild(needPost);
@@ -28,9 +29,9 @@
     posts.forEach(post => {
       const postCard = document.createElement('a');
       postCard.className = 'posting-card';
-      postCard.href = `/post1/${post.post_id}`;  // 수정: post.id -> post.post_id
+      postCard.href = `/post1/${post.post_id}`; // post_id를 사용
       postCard.innerHTML = `
-        <div id='posttitle1'>${post.post_title}</div>  <!-- 수정: post.postTitle -> post.post_title -->
+        <div id='posttitle1'>${post.post_title}</div>
         <div class="posting-wrapper">
           <div class="posting-info">
             <div class="like-post">좋아요 0</div>
@@ -38,7 +39,7 @@
             <div class="view-post">조회수 0</div>
           </div>
           <div class="posting-time">
-            <div id="postingdate1">${post.upload_dt}</div>  <!-- 수정: post.uploaddate -> post.upload_dt -->
+            <div id="postingdate1">${post.upload_dt}</div>
           </div>
         </div>
         <div class= "cw-container">
